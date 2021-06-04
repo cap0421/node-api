2d28c689f60301310168461bcf281651bff6e08d
ikwk <anayndiwakun@gmail.com>
2021-06-05 01:42:10 +0800

    regis user without validation

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+//Import Routes
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+//Coonect to DB
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('connected to db'));
+
+//Middleware
+app.use(express.json());
+
+//Route middleware
+app.use('/api/user', authRoute);
+app.listen(3000, () => console.log('Server Up and Running'));
