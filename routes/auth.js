ab78011eccab16d0c1bec6345dd7cee0bcf21e37
ikwk <anayndiwakun@gmail.com>
2021-06-05 02:35:39 +0800

    added hash password to regist

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,27 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const {registerValidation} = require('../validation');
-
 
 
 router.post('/register', async (req, res) => {
   //Validate Data Before make user
   const {error} = registerValidation(req.body);
   if(error) return res.status(400).send(error.details[0].message);
+
+  //Check if the user is already in the database
+  const emailExist = await User.findOne({email: req.body.email});
+  if(emailExist) return res.status(400).send('Email already exists');
+
+  //Hash passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
   
+  //Create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassword
   });
   try{
     const savedUser = await user.save();
